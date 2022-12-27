b361511daed4b13a2fea5ee88339573cdce41c59
Thomas Borghi <tlx.borghi@gmail.com>
2022-12-27 12:53:30 -0800

    support both GET and POST methods

--- LedControlling/ledControllerApi.php
+++ LedControlling/ledControllerApi.php
@@ -22,44 +22,46 @@
  * 
  */
 	require 'Leds.php';
+	$serverVars = [];
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		//echo "Received";
-		//print_r($_POST);
-		//echo $_POST["blueLedStatus"];
-		if ($_POST["toDo"] == "setLed") {
-			echo "setting Leds";
-			if ($_POST["blueLedStatus"] == "true") {
-				setLed("blue", 1);
-			} else {
-				setLed("blue", 0);
-			}
-			if ($_POST["greenLedStatus"] == "true") {
-				setLed("green", 1);
-			} else {
-				setLed("green", 0);
-			}
+		$serverVars = $_POST;
+	} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+		$serverVars = $_GET;
+	}
+	if ($serverVars["toDo"] == "setLed") {
+		echo "setting Leds";
+		if ($serverVars["blueLedStatus"] == "true") {
+			setLed("blue", 1);
 		} else {
-			$blueLedStatus = getLedStatus("blue");
-			$greenLedStatus = getLedStatus("green");
-			if ($blueLedStatus) {
-				$blueLedStatus = "true";
-			} else {
-				$blueLedStatus = "false";
-			}
-			if ($greenLedStatus) {
-				$greenLedStatus = "true";
-			} else {
-				$greenLedStatus = "false";
-			}
-			$LedStatus = [
-				"blueLedStatus" => $blueLedStatus,
-				"greenLedStatus" => $greenLedStatus,
-			];
-			//echo $blueLedStatus;
-			//echo "          ";
-			//echo $greenLedStatus;
-			echo json_encode($LedStatus);
+			setLed("blue", 0);
 		}
+		if ($serverVars["greenLedStatus"] == "true") {
+			setLed("green", 1);
+		} else {
+			setLed("green", 0);
+		}
+	} else if ($serverVars["toDo"] == "updatePage" ) {
+		$blueLedStatus = getLedStatus("blue");
+		$greenLedStatus = getLedStatus("green");
+		if ($blueLedStatus) {
+			$blueLedStatus = "true";
+		} else {
+			$blueLedStatus = "false";
+		}
+		if ($greenLedStatus) {
+			$greenLedStatus = "true";
+		} else {
+			$greenLedStatus = "false";
+		}
+		$LedStatus = [
+			"blueLedStatus" => $blueLedStatus,
+			"greenLedStatus" => $greenLedStatus,
+		];
+		//echo $blueLedStatus;
+		//echo "          ";
+		//echo $greenLedStatus;
+		header("Content-Type: application/json; charset=utf-8");
+		echo json_encode($LedStatus);
 	}
 
 
